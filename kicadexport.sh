4ef278a74123afd63f2423d202b971b708f28503
zeulewan <zeul@mordasiewicz.com>
2025-01-06 13:22:29 -0500

    update action and shell script

--- kicadexport.sh
+++ kicadexport.sh
@@ -9,12 +9,15 @@ docker pull kicad/kicad:8.0
 
 # Run KiCad Docker container to export files
 echo "Exporting schematic, PCB, and 3D model..."
+sudo chmod -R 777 .
 docker run --rm -v "$(pwd):/workspace" kicad/kicad:8.0 bash -c "
   cd /workspace
 
   # Export schematic
   mkdir -p images
-  kicad-cli sch export svg -o 'images/sch.svg' kicad/*.kicad_sch
+  kicad-cli sch export svg -o 'images/sch' kicad/*.kicad_sch
+  mv images/sch/*.svg images/sch.svg
+  rm -r images/sch
 
   # Export PCB
   kicad-cli pcb export svg -o 'images/pcb.svg' --page-size-mode 2 --exclude-drawing-sheet --layers 'F.Cu,F.SilkS,F.Mask' kicad/*.kicad_pcb
